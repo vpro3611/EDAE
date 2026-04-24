cc28535960674b4f2a95587cdfc52ede89d4e2be
vpro3611 <vprov8645@gmail.com>
2026-04-24 14:54:21 +0200

    add: added auth middlware to private router, in order for token to be validated

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import {DepsContainer} from "./container";
 import express, {Express} from "express";
 import cookieParser from "cookie-parser";
+import {authMiddleware} from "./modules/middlewares/middleware.auth";
 
 export function createApp(dependencies: DepsContainer): Express {
     const app = express();
@@ -16,11 +17,15 @@ export function createApp(dependencies: DepsContainer): Express {
     app.use("/pub", publicRouter);
     app.use("/protected", privateRouter);
 
+    // using auth middleware for all routes in privateRouter, so that the bearer
+    // token is validated before the route handler is called
+    privateRouter.use(authMiddleware(dependencies.jwtTokenService));
+
     publicRouter.get("/health", (req, res) => {
         res.status(200).json({message: "OK"});
     }); //
 
 
-    
+
     return app;
 }
