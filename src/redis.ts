6f6c369b699542683c6bfc6668feb52d44a5e6fc
vpro3611 <vprov8645@gmail.com>
2026-05-09 21:57:30 +0200

    update: updated redis to use credentials from env

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,10 @@
 import Redis from 'ioredis'
 
-export const REDIS = new Redis();
+export const REDIS = new Redis({
+    host: process.env.REDIS_HOST ?? 'localhost',
+    port: Number(process.env.REDIS_PORT ?? 6379),
+    ...(process.env.REDIS_PASSWORD ? { password: process.env.REDIS_PASSWORD } : {}),
+});
 
 
 
