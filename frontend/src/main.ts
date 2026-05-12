2edb376e7cc3600fb55803623ca9b68309065b9c
vpro3611 <vprov8645@gmail.com>
2026-05-12 15:18:43 +0200

    feat: add Google login button and integration to frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import GoogleSignInPlugin from 'vue3-google-signin'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(GoogleSignInPlugin, {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+})
 app.mount('#app')
